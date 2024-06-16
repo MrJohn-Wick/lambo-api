7be7bfba094afc0cc7ba878bfe4099b351f9036d
ashibeko <ashibeko@gmail.com>
2024-06-16 23:18:57 +0300

    remove old code

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ import { swaggerDocs } from '@lambo/routes/swagger';
 dotenv.config();
 dotenv.config({ path: `.env.local`, override: true });
 
-const middleware = require('./middleware');
 const app: Express = express();
 const port = Number(process.env.PORT) || 3000;
 
@@ -15,11 +14,6 @@ app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send("<h1>It's work!!!</h1>");
-});
-
-// post Generate request
-app.post('/generate', middleware.generateToken, (req, res) => {
-  res.status(200).send(res.locals.signature);
 });
 
 app.use('/user', userRouter);
