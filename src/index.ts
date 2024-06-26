224e6df06e519e1ab57875df0f301a0c079be0b1
ashibeko <ashibeko@gmail.com>
2024-06-26 10:06:51 +0300

    added dotenv-flow

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,15 @@
 import 'module-alias/register';
 import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import dotenvFlow from 'dotenv-flow';
 import userRouter from '@lambo/routes/users';
 import { swaggerDocs } from '@lambo/routes/swagger';
 
-dotenv.config();
-dotenv.config({ path: `.env.local`, override: true });
+dotenvFlow.config();
 
 const app: Express = express();
 const port = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
-
-app.get('/', (req: Request, res: Response) => {
-  res.send("<h1>It's work!!!</h1>");
-});
 
 app.use('/user', userRouter
   // #swagger.tags = ['Users']
