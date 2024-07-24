c20a985016e5b375712c0935e456e16753330a1e
Alexander Shibeko <ashibeko@gmail.com>
2024-07-24 22:08:18 +0300

    google auth

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,21 @@ import passport from 'passport';
 import { authController } from './controllers/auth';
 import { oauthServer } from './passport';
 import { profileController } from './controllers/profile';
+import cors from 'cors';
+import { generateToken } from './utils/auth';
+import { storeTokens } from './repositories/tokens';
+import { User } from '@prisma/client';
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.json({
-    status: "Ok"
+    status: "Server ok"
   });
 });
 
@@ -26,6 +31,26 @@ app.post(
   '/oauth/token',
   oauthServer.token(),
   oauthServer.errorHandler()
+);
+
+app.get(
+  '/oauth/provider/google',
+  passport.authenticate('google-token', { session: false }),
+  async (req: Request, res: Response) => {
+    const aToken = generateToken();
+    const rToken = generateToken();
+    const expiresAt = new Date(new Date().getTime() + 60*60*1000);
+    if (req.user) {
+      await storeTokens(req.user as User, aToken, rToken, expiresAt);
+      res.json({
+        token_type: 'Bearer',
+        access_token: aToken,
+        refresh_token: rToken,
+        expires_in: expiresAt,
+      });
+    }
+    res.status(401);
+  }
 );
 
 app.get(
--- src/passport.ts
+++ src/passport.ts
@@ -1,10 +1,11 @@
 import { compare } from 'bcryptjs';
 import oauth2orize from 'oauth2orize';
 import passport from 'passport';
-import { Strategy as BearerStrategy } from 'passport-http-bearer';
+import { Strategy as BearerStrategy, Strategy } from 'passport-http-bearer';
 import { generateToken, isValidToken } from './utils/auth';
 import { deleteRefreshToken, getAccessToken, getRefreshToken, storeTokens } from './repositories/tokens';
-import { getUserByEmail } from './repositories/users';
+import { createUser, getUserByEmail } from './repositories/users';
+import { Issuer } from 'openid-client';
 
 
 export const oauthServer = oauth2orize.createServer();
@@ -12,7 +13,7 @@ const accessTokenLifetime = 60 * 60 * 1000; //one hour
 
 oauthServer.exchange(
   oauth2orize.exchange.password(async (client, username, passport, scope, done) => {
-    console.log('Search user');
+    console.log('Search user', username);
     const user = await getUserByEmail(username);
     if (user && user.passwordHash && await compare(passport, user.passwordHash)) {
       const accessToken = generateToken();
@@ -36,16 +37,40 @@ oauthServer.exchange(oauth2orize.exchange.refreshToken(async (client, refreshTok
     const rToken = generateToken();
     const expiresAt = new Date(new Date().getTime() + accessTokenLifetime);
     await storeTokens(token.user, aToken, rToken, expiresAt);
-    done(null, aToken, rToken, { expires_in: expiresAt });
+    return done(null, aToken, rToken, { expires_in: expiresAt });
   }
-  done(null, false);
+  return done(null, false);
 }));
 
 passport.use(new BearerStrategy(async (token: string, done) => {
   const storedToken = await getAccessToken(token);
   console.log(storedToken);
   if ( storedToken && isValidToken(storedToken)) {
-    done(null, storedToken.user);
+    return done(null, storedToken.user);
   }
-  done(null, false);
+  return done(null, false);
+}));
+
+passport.use('google-token', new BearerStrategy( async (token: string, done) => {
+  console.log("Google authorization");
+  const googleIssuer = await Issuer.discover('https://accounts.google.com');
+  const client = new googleIssuer.Client({
+    client_id: '856788290766-7m656jj3ppsfr2flr3blotasu59lpupj.apps.googleusercontent.com',
+  });
+  
+  const userInfo = await client.userinfo(token);
+
+  if (userInfo.email) {
+    try {
+      const user = await getUserByEmail(userInfo.email);
+      if (user) {
+        return done(null, user);
+      }
+      const newUser = await createUser(userInfo.email);
+      return done(null, newUser);
+    } catch (error) {
+      done(error, false); 
+    }
+  }
+  return done(null, false);
 }));
