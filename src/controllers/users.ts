5e914f3cac9cb8421c928eec3b20d2579f50ded4
ashibeko <ashibeko@gmail.com>
2024-08-24 15:07:32 +0300

    api responses templates

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,13 +1,12 @@
 import { Request, Response } from 'express';
 import { getUsers } from '../repositories/users';
+import { apiSuccessResponse } from '../utils/responses';
 
 export const usersController = {
   async list(req: Request, res: Response) {
     const limit = req.query.limit ? Number(req.query.limit) : 0;
     const users = getUsers(limit);
-    res.json({
-      success: true,
-      payload: users
-    });
+
+    res.json(apiSuccessResponse(users));
   }
 }
