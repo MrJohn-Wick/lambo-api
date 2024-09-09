7d8fac727b7432500db760f2332d92b5b87614bf
ashibeko <ashibeko@gmail.com>
2024-09-09 22:14:43 +0300

    change database to mysql

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,8 @@ npx dotenv-flow npx prisma migrate deploy
 # npx dotenv-flow npm run seed/categories
 # npx dotenv-flow npm run seed
 
-npm run swagger
+npx dotenv-flow npm run swagger
 
-npm run build
+npm dotenv-flow npm run build
 
 systemctl restart lambo-api.service
