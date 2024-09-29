8a1ef6a23d339ec77a56e9a291c5b1d97c3ce406
ashibeko <ashibeko@gmail.com>
2024-09-29 22:07:48 +0300

    users settings migration

---
+++ prisma/migrations/20240929185131_users_settings/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `users_settings` (
+    `id` VARCHAR(191) NOT NULL,
+    `key` VARCHAR(191) NOT NULL,
+    `value` VARCHAR(191) NOT NULL,
+    `user_id` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `users_settings` ADD CONSTRAINT `users_settings_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
