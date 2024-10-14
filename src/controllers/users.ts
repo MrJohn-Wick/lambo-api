d9271848d7695f33e1b685a1e7fbfbced9f35c5a
Shibeko Alexander <ashibeko@gmail.com>
2024-10-14 23:22:56 +0300

    fix empty avatar (#76)

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -34,7 +34,7 @@ export const usersController = {
     let avatar = null;
     if (profile && profile.gallery) {
       const gallery = await getGallery(profile.gallery.id);
-      const key = gallery?.items[0].key;
+      const key = gallery?.items.length ? gallery?.items[0].key : null;
       if (key) {
         avatar = getS3PublicKey(key);
       }
